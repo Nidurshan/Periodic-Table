306e6e69de765deb19ea7e73f6de6b3983c56a80
Nidurshan <nidurshanpro@gmail.com>
2023-11-09 22:11:01 +0530

    first commit

---
+++ home/script.js
@@ -0,0 +1,28 @@
+var hBtn = document.getElementById("hBox");
+var heBtn = document.getElementById("heBox");
+var liBtn = document.getElementById("liBox");
+var beBtn = document.getElementById("beBox");
+var bBtn = document.getElementById("bBox");
+
+var cBtn = document.getElementById("cBox");
+var nBtn = document.getElementById("nBox");
+var oBtn = document.getElementById("oBox");
+var fBtn = document.getElementById("fBox");
+var neBtn = document.getElementById("neBox");
+
+var naBtn = document.getElementById("naBox");
+var mgBtn = document.getElementById("mgBox");
+var alBtn = document.getElementById("alBox");
+var siBtn = document.getElementById("siBox");
+var pBtn = document.getElementById("pBox");
+
+var sBtn = document.getElementById("sBox");
+var clBtn = document.getElementById("clBox");
+var arBtn = document.getElementById("arBox");
+var kBtn = document.getElementById("kBox");
+var caBtn = document.getElementById("caBox");
+
+hBtn.addEventListener("click", () => {
+    alert("You clicked the hydrogen!");
+});
+
